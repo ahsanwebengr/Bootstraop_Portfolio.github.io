2d7701d5b4beb54c812699b4c25b6879be9e0673
Muhammad Ahsan <iamahsan512@gmail.com>
2022-06-08 00:59:55 -0700

    Upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//Navbar Hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
